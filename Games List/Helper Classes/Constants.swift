b6e2769125aec6defdd5750cbf7aaf7af7c16ab3
Prashant Singh <psin0015@student.monash.edu>
2022-01-23 19:23:24 +0530

    Added app icon, launch image, app theme colour, UI test case, and readme file.

--- Games List/Helper Classes/Constants.swift
+++ Games List/Helper Classes/Constants.swift
@@ -9,3 +9,4 @@ import UIKit
 
     let gamesTableViewCellIdentifier = "gameTableViewCell"
     let gamesListURL = "https://api.unibet.com/game-library-rest-api/getGamesByMarketAndDevice.json?jurisdiction=UK&brand=unibet&deviceGroup=mobilephone&locale=en_GB&currency=GBP&categories=casino,softgames&clientId=casinoapp"
+    let appThemeColor:UIColor = .systemRed
