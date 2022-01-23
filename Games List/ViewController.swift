2e34231c2b1191981ac8d8d0fa2f65309e9be497
Prashant Singh <psin0015@student.monash.edu>
2022-01-23 15:58:16 +0530

    Initial Commit

---
+++ Games List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Games List
+//
+//  Created by Prashant Singh on 23/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
