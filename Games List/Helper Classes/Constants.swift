317ca6c5e0acee8ffbb2ee811d1f88af78d7537c
Prashant Singh <psin0015@student.monash.edu>
2022-01-23 16:56:05 +0530

    Created project structure, custom table view cells and related table view logic.

---
+++ Games List/Helper Classes/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Games List
+//
+//  Created by Prashant Singh on 23/1/22.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}
---
+++ Games List/View/CustomCells/GameTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GameTableViewCell.swift
+//  Games List
+//
+//  Created by Prashant Singh on 23/1/22.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
